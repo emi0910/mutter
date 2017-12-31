ca50326b2139651a75080e2da81fa3c9666fbe7a
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-01 05:25:02 +0900

    Validate user columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 50}, uniqueness: true
+  validates :password, presence: true, length: {minimum: 5, maximum: 255},
+            format: {with: /\A[a-z\d]+\z/i}
+  has_secure_password
 end
