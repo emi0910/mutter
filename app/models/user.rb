cb0763bc061f9a470d3c19586f37e411425ca5ec
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-02 22:52:29 +0900

    Add test for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
             format: {with: VALID_NAME_REGEXP}
   validates :password, presence: true, length: {minimum: 5, maximum: 255}
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
