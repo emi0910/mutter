17935f391ddd30321c5035c398dd3feca7e7994a
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-02 21:21:00 +0900

    Create login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   resources :users, :except => [:create, :new]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
