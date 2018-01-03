e7b0ba73d1bb537b6cf30367b1274575f85815c8
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-03 17:14:03 +0900

    Remember and forget session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,34 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_NAME_REGEXP = /\A[a-z\d][a-z\d\-_]+\z/i
   validates :name, presence: true, length: {maximum: 50}, uniqueness: true,
             format: {with: VALID_NAME_REGEXP}
   validates :password, presence: true, length: {minimum: 5, maximum: 255}
   has_secure_password
 
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231195212) do
+ActiveRecord::Schema.define(version: 20180102175731) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
