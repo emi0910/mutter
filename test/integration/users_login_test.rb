aebdbe6a4abf0615546b8726fcbfe5c628ec1717
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-04 17:28:48 +0900

    Add test for permanet session

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,8 +7,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with valid information followed by logout" do
     get login_path
-    post login_path, params: {session: {name: @user.name,
-                                        password: 'password'}}
+    log_in_as(@user)
+    assert_not_empty cookies['remember_token']
+
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
@@ -18,9 +19,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     delete logout_path
     assert_not is_logged_in?
+    assert_empty cookies['remember_token']
     assert_redirected_to root_url
 
-    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
