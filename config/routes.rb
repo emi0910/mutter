c92550c32a4f9d40b974b1d42a94e12cd6c7f69f
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-05 03:43:14 +0900

    Fix bug that it shows duplicated users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get  '/home',   to: 'welcome#index'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  resources :users, :except => [:create, :new]
+  resources :users, :except => [:new]
 
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
