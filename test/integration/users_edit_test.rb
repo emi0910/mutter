0f974ee41519a76473293145c87598ed2ceea72b
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-05 03:28:52 +0900

    Add index action for users

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,16 +3,19 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:example)
+    @other = users(:other)
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
-    patch user_path(@user), params: {user: {name: "",
-                                            password: "foo",
-                                            password: "bar"}}
+    patch user_path(@user), params: {user: { name: "",
+                                             password: "foo",
+                                             password: "bar"}}
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     name = "FooBar"
     patch user_path(@user), params: { user: { name:  name,
@@ -22,5 +25,45 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     @user.reload
     assert_equal name, @user.name
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other)
+    patch user_path(@user), params: { user: { name: @user.name } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name  = "FooBar"
+    patch user_path(@user), params: { user: { name:  name,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
   end
 end
