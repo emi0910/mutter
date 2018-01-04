bc1ca108233114687193982f6963dfab64a56691
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2018-01-05 05:53:35 +0900

    Add nickname column for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :nickname, :password,
+                                 :password_confirmation)
   end
 
   def logged_in_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104180832) do
+ActiveRecord::Schema.define(version: 20180104203128) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180104180832) do
     t.datetime "updated_at", null: false
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "nickname"
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
